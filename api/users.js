546de186dbd5f732b61f1fb1dea135d6d18e25f7
zajdnik <ajdnikz@gmail.com>
2015-05-21 18:25:43 +0200

    Added gitignore

---
+++ api/users.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var router = express.Router();
+
+var db = require('../mongoConnection');
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/Register', function(req, res){
+	req.body.username;
+	req.body.password;
+});
+
+router.post('/Login', function(req, res){
+	var username = req.body.username;
+	var password = req.body.password;
+	var gcm_id = req.body.gcm_id;
+
+	var user = {};
+	user.username = username;
+	user.password = password;
+	user.gcm_id = gcm_id;
+	db.InsertNewUser(user,function(error){
+		if(error)
+		{
+			return res.status(500);
+		}
+		else
+		{
+			return res.status(201);
+		}
+
+	});
+
+});
+
+router.post('/',function(req,res){
+	// GCM push, to create pin
+
+});
+
+router.post('/LoginPin', function(req,res){
+
+});
+
+module.exports = router;
